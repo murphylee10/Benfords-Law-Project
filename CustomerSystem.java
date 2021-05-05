67436f6c3f0f54e0d704a057c22ba247acf14347
Murphy Lee <murphylee2004@gmail.com>
2021-05-05 18:37:39 -0400

    Small tweaks with base program

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -5,14 +5,8 @@
  * Description: A customer sales information that makes use of Benford's Law and Java APIs
  * */
 
-import java.util.Scanner;
-
 class CustomerSystem {
     public static void main(String[] args) {
-        // Instantiate Scanner
-        Scanner reader = new Scanner(System.in);
-
-
-        reader.close();
+        
     }
 }
