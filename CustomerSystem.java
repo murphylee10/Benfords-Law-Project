4cb402d09b7ffc4abf8996eaf0e1530a69f51a3e
Murphy Lee <murphylee2004@gmail.com>
2021-05-05 18:15:22 -0400

    Create repository

---
+++ CustomerSystem.java
@@ -0,0 +1,18 @@
+/*
+ * Date: May 5, 2021
+ * Names: Naomi Mezheritsky and Murphy Lee
+ * Teacher: Mr. Ho
+ * Description: A customer sales information that makes use of Benford's Law and Java APIs
+ * */
+
+import java.util.Scanner;
+
+class CustomerSystem {
+    public static void main(String[] args) {
+        // Instantiate Scanner
+        Scanner reader = new Scanner(System.in);
+
+
+        reader.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Benford's-Law-Assignment
+
+A sales analysis program designed to take customer sales information and check for fraud using Benford's Law - intended to aid retail companies with the analysis of sales.
+
+A project developed for ICS3U (Computer Science 11)
