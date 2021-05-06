6ed60e6a43d0319cea8463e0bb1e5b4159a8af1f
Murphy Lee <murphylee2004@gmail.com>
2021-05-06 18:56:34 -0400

    Create methods and options menu

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -5,8 +5,60 @@
  * Description: A customer sales information that makes use of Benford's Law and Java APIs
  * */
 
+import java.util.Scanner;
+
 class CustomerSystem {
     public static void main(String[] args) {
-        
+        String userChoice;           // Which menu option the user has chosen
+        boolean keepGoing = true;    // Ensures that the while loop runs at least once
+
+        // Instantiate Scanner
+        Scanner reader = new Scanner(System.in);
+
+        // Loop through user options as long as the user wants to keep going
+        while (keepGoing == true) {
+            // Print user options
+            printMenu();
+            userChoice = reader.nextLine();
+            
+            // Prompt the user for re-input as long as their choice is invalid (not a number from 1 - 3)
+
+            // If the user entered 1, read the sales data
+
+            // If the user entered 2, 
+
+            // If the user entered 3, set the keepGoing variable to false
+
+        }
+
+        reader.close();
+    }
+
+    /* 
+     * Description: 
+     * 
+     * @author - 
+     * 
+     * */
+    public static void printMenu() {
+
+    }
+
+    /*
+     * Description:
+     * 
+     * @author - Naomi Mezheritsky
+     * */
+    public static void getSalesData() {
+
+    }
+
+    /*
+     * Description:
+     * 
+     * @author - Murphy Lee
+     * */
+    public static void checkSales() {
+
     }
 }
