267432340260f6b47bb696cf0d6d8542f8b753f0
Murphy Lee <murphylee2004@gmail.com>
2021-05-10 18:02:54 -0400

    Implemented checkSales function

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -7,6 +7,7 @@
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import jdk.net.NetworkPermission;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,6 +23,8 @@ class CustomerSystem {
 
         // Instantiate Scanner
         Scanner reader = new Scanner(System.in);
+
+        checkSales(reader);
 
         // Loop through user options as long as the user wants to keep going
         while (keepGoing == true) {
@@ -72,19 +75,19 @@ class CustomerSystem {
      * @param scanner - The Scanner used to read from the file
      * @throws FileNotFoundException - Exception raised when the program tries to read the CSV, thrown to main
      * */
-    public static void checkSales(Scanner scanner) throws FileNotFoundException {
+    public static double[] checkSales(Scanner scanner) throws FileNotFoundException {
         int totalDigits = 0;            // Accumulator variable to help calculate frequency
-        int[] firstDigits = new int[9];   // Stores the number of occurences of each first digit
+        int[] firstDigits = new int[9];   // Stores the number of occurences of each first  
+        double[] digitFrequency = new double[9];
         String fileName = "sales.csv";    // The file name of the CSV
         String line;                      // Stores each line of the file
         Scanner lineReader;               // Scanner takes in each line of the CSV
         int salesNum;                     // Stores each sales number
-        int digit;                        // Stores each digit
+        int digit;                        // Stores each first digit
 
         // Open the CSV file into the Scanner
         File file = new File(fileName);
         scanner = new Scanner(file);
-
 
         scanner.nextLine();    // Skip the first line of the CSV (spreadsheet headers)
 
@@ -101,16 +104,63 @@ class CustomerSystem {
 
             // Pass the sales value into nextInt
             salesNum = lineReader.nextInt();
-            System.out.println(salesNum);
 
+            digit = getFirstDigit(salesNum);
+
+            // Increase the corresponding value of the array by 1
+            firstDigits[digit - 1]++;
+
+            totalDigits++;           // Add 1 to the accumulator vairable
         }
         // Calculate the frequency of each first digit
+        digitFrequency = itemFrequency(firstDigits, totalDigits);
 
-        // Store frequencies in an array
+        // Display to terminal whether fraud occured - Depends on the frequency of the digit 1
+        System.out.println("The first digit frequency of the number 1 is " + digitFrequency[0] + "%");
+        if (digitFrequency[0] >= 29 && digitFrequency[0] <= 32) {
+            System.out.println("Fraud has most likely not occured :)");
+        }
+        else {
+            System.out.println("Fraud has occured");
+        }
 
-        // Display to terminal whether fraud occured
+        // Export results to a seperate CSV file
 
         // Return array
+        return digitFrequency;
+    }
+
+    /*
+     * Description: Finds the first digit of any integer
+     * 
+     * @author - Murphy Lee
+     * @param num - The number whose first digit needs to be found
+     * @return num - The first digit
+     * */
+    public static int getFirstDigit(int num) {
+        // Loop while the number is greater than 10
+        while (num >= 10) {
+            num /= 10;
+        }
+        // Return the digit
+        return num;
+    }
+
+    /*
+     * Description: Finds the percentage of each item in an array, and returns them as a seperate array
+     * 
+     * @author - Murphy Lee
+     * @param arr - Array containing each items numbers
+     * @param total - The sum of all numbers in the array
+     * @return newArr - Array containing frequencies of each item (doubles)
+     * */
+    public static double[] itemFrequency(int[] arr, int total) {
+        double[] newArr = new double[arr.length];    // Create new array with the same length as the param array
+
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = ((1.0 * arr[i]) / total) * 100;     // Divide num / total and multiply by 100%
+        }
+        return newArr;
     }
 
     /*
